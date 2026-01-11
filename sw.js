50f85de68f79ca470b1b91963fc050e3f7fc68e1
ronmailx-boop <ronmailx@gmail.com>
2026-01-11 19:27:24 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,3 +1,38 @@
-self.addEventListener('fetch', function(event) {
-    // מאפשר התקנה כאפליקציה (PWA)
+const CACHE_NAME = 'vplus-cache-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './style.css',
+  './script.js',
+  './manifest.json',
+  './icon.png'
+];
+
+// התקנת ה-Service Worker ושמירת קבצים במטמון
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+// הפעלת ה-Service Worker וניקוי מטמון ישן
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
+      );
+    })
+  );
+});
+
+// שליפת קבצים מהמטמון כשאין אינטרנט
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
 });
