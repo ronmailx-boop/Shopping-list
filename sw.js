227c891d931a5be24e2d2d109b14c9dbaca52a73
ronmailx-boop <ronmailx@gmail.com>
2026-01-11 19:37:11 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,38 +1,10 @@
-const CACHE_NAME = 'vplus-cache-v1';
-const ASSETS = [
-  './',
-  './index.html',
-  './style.css',
-  './script.js',
-  './manifest.json',
-  './icon.png'
-];
+const CACHE_NAME = 'vplus-v2';
+const assets = ['./', './index.html', './style.css', './script.js', './manifest.json'];
 
-// התקנת ה-Service Worker ושמירת קבצים במטמון
-self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(ASSETS);
-    })
-  );
+self.addEventListener('install', (e) => {
+    e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(assets)));
 });
 
-// הפעלת ה-Service Worker וניקוי מטמון ישן
-self.addEventListener('activate', (event) => {
-  event.waitUntil(
-    caches.keys().then((keys) => {
-      return Promise.all(
-        keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
-      );
-    })
-  );
-});
-
-// שליפת קבצים מהמטמון כשאין אינטרנט
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
-  );
+self.addEventListener('fetch', (e) => {
+    e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
 });
